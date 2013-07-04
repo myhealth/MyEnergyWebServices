3b053c0ed3eb6ebc2c84b2f3fea7e97902750b2d
Arsénio Costa <arsenio.costa@ecsaude.uminho.pt>
2013-07-04 12:28:43 +0100

    first commit

---
+++ MyEnergyWS/Controllers/ValuesController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using BLToolkit.Data;
+using MyEnergyWS.Models;
+
+namespace MyEnergyWS.Controllers
+{
+    public class ValuesController : ApiController
+    {
+        // GET api/values
+        public IEnumerable<FoodCategory> Get()
+        {
+            List<FoodCategory> result = new List<FoodCategory>();
+
+            using (DbManager db = new DbManager("MyEnergy"))
+            {
+
+                result = db.SetSpCommand("GetFoodCategories").ExecuteList<FoodCategory>();
+
+            }
+
+            return result;
+        }
+
+        // GET api/values/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/values/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ MyEnergyWS/Models/FoodCategories.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MyEnergyWS.Models
+{
+    public class FoodCategory
+    {
+        public String name { get; set; }
+    }
+}
