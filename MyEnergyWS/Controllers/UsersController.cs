8077dd47a82810f48024c87621effbb557c86845
Arsenio Costa <arsenio.costa@gmail.com>
2013-07-09 16:49:31 +0100

    new controller and module for users

---
+++ MyEnergyWS/Controllers/UsersController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using BLToolkit.Data;
+using MyEnergyWS.Models;
+
+namespace MyEnergyWS.Controllers
+{
+    public class UsersController : ApiController
+    {
+        // GET api/values
+        public IEnumerable<User> Get(int index, int count = 50)
+        {
+            List<User> result = new List<User>();
+
+            using (DbManager db = new DbManager("MyEnergy"))
+            {
+
+                result = db.SetSpCommand("GetUsers",
+                    db.Parameter("@index", index),
+                    db.Parameter("@count", count)
+                   ).ExecuteList<User>();
+                    
+
+            }
+
+            return result;
+        }
+
+        // GET api/values/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/values/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ MyEnergyWS/Models/User.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MyEnergyWS.Models
+{
+    public class User
+    {
+        public UInt64 id { get; set; }
+        public String nome { get; set; }
+    }
+}
