1ddd8f3879967a0dc3e79f0250e235e7b198cc8c
Arsenio Costa <arsenio.costa@gmail.com>
2013-07-10 11:16:11 +0100

    added database project to solution

---
+++ Database/MyEnergyDatabase/Schema Objects/Database Level Objects/user.table.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[user]
+(
+	[user_id] BIGINT NOT NULL, 
+	birthdate DATE NOT NULL,
+	creation_date DATE NOT NULL,
+	gender CHAR CHECK (gender IN ('m', 'f')) DEFAULT 'm',
+	PRIMARY KEY([user_id])
+)
