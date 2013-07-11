fbd0e1d3f9a00629ae2ef72e67adadf63a071e2b
Arsenio Costa <arsenio.costa@gmail.com>
2013-07-11 10:35:57 +0100

    added category table which represents a food category and subcategory

--- Database/MyEnergyDatabase/Schema Objects/Schemas/dbo/Tables/food_category.table.sql
+++ Database/MyEnergyDatabase/Schema Objects/Schemas/dbo/Tables/food_category.table.sql
@@ -1,9 +1,25 @@
-﻿CREATE TABLE [dbo].[food_category]
+﻿/**
+	This table represents both a food category and a food subcategory
+	it can be used to model a tree with N levels but we're only going
+	to use it for ONE and ONE level only!
+				Category ---> Subcategory
+		ex:		category(id => 1, name => Fruits and Vegetables, category_id => NULL, ...) 
+			 	subcategory(id = 2, name =>	Apple, category_id => NULL, ...)	
+	
+*/
+
+CREATE TABLE [dbo].[food_category]
 (
 	[id] INT NOT NULL IDENTITY, 
+	[category_id] INT NULL, 
 	[name] NVARCHAR(512) NOT NULL UNIQUE,
 	[description] NVARCHAR(MAX),
 
 
-	CONSTRAINT pk_category PRIMARY KEY(id)	
+	CONSTRAINT pk_category PRIMARY KEY(id),
+	
+	CONSTRAINT fk_category_subcategory 
+		FOREIGN KEY ([category_id]) REFERENCES [food_category](id)
+
+	
 )
--- Database/MyEnergyDatabase/Schema Objects/Schemas/dbo/Tables/food_subcategory.table.sql
+++
@@ -1,9 +0,0 @@
-﻿CREATE TABLE [dbo].[food_subcategory]
-(
-	[id] INT NOT NULL IDENTITY, 
-	[name] NVARCHAR(512) NOT NULL UNIQUE,
-	[description] NVARCHAR(MAX),
-
-
-	CONSTRAINT pk_subcategory PRIMARY KEY(id)	
-)
